f9ffa826316db7e5790b0bb140ee7489a6258047
leetakano <80697641+leetakano@users.noreply.github.com>
2022-04-21 11:23:20 -0500

    Update

---
+++ intothewood/vcquestion.js
@@ -0,0 +1,39 @@
+
+
+// Array of Objects
+const quiz = [
+    {
+        q: 'This drawing shows...',
+        options: ['Storied cambium','Non-storied cambium','Uniseriate ray initial','Multiseriate'],
+        answer:0,
+        img: 'images/scambium.png'
+    },
+    {
+        q: 'How many ray initials does this drawing show?',
+        options: ['One','Six','Seven','Four'],
+        answer:2,
+        img: 'images/uniray.png'
+    },
+    {
+        q: 'This drawing shows...',
+        options: ['Non-storied cambium','Multiseriate ray initial', 'Uniseriate ray initial', 'Fusiform initial'],
+        answer:1,
+        img: 'images/multiray.png'
+    },
+    {
+        q: 'Vascular cambium primarily undergoes...',
+        options: ['Apoptosis or cell death', 'Tangential cell division', 'Meiotic cell division', 'Longitudinal cell division'],
+        answer:1
+    },
+    {
+        q: 'This drawing shows...',
+        options: ['Non-storied cambium','Multiseriate', 'Uniseriate', 'Fusiform initial'],
+        answer:0,
+        img: 'images/nscambium.png'
+    },
+    {
+        q: 'How many types of fusiform initials are there?',
+        options: ['One','Two', 'Three', 'Four'],
+        answer:1
+    },
+]
