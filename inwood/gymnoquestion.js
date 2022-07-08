c2b681df6806c105b9de72ea65f942039c475a47
Grace LeeTakano <80697641+leetakano@users.noreply.github.com>
2022-07-07 22:40:49 -0500

    Add files via upload

---
+++ inwood/gymnoquestion.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        q: 'Gymnosperms have...',
+        options: ['Compression wood','Rough wood','Tension wood','Moist wood'],
+        answer:0
+    },
+    {
+        q: 'Gymnosperms do NOT have...',
+        options: ['Resin canals','Seeds','Vessel elements','Secondary growth'],
+        answer:2
+    },
+    {
+        q: 'Gymnosperms do NOT have...',
+        options: ['Leaves','Ovaries', 'Resin canals', 'Homogenous tissues'],
+        answer:1
+    },
+    {
+        q: 'Compression wood is a strategy to...',
+        options: ['Keep the tree upright', 'Be petty', 'Fight the strong wind', 'Fight the cliff'],
+        answer:0
+    },
+    {
+        q: 'Which of the following is a gymnosperm?',
+        options: ['Pines','Blueberries', 'Duckweed', 'Orchid'],
+        answer:0,
+    }
+]
