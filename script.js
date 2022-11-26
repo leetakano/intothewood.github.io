923eed6f3a58208578fb3d706069432235b05650
Grace LeeTakano <80697641+leetakano@users.noreply.github.com>
2022-11-25 22:54:36 -0600

    update

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var flky = new Flickity('.carousel', {
     groupCells: false,
     // group cells together in slides
+    pageDots: false,
+    wrapAround: true
+
+});
+
+var select = document.querySelector('select')
+select.addEventListener('change', function (event) {
+    console.log(event.target.value)
+    flky.select(event.target.value - 1)
 });
